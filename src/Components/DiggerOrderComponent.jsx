89237f4009ad47d01e5b97d4f424d8eda710a0a9
Mr.Amperage <Mr.Amperage@protonmail.com>
2021-07-26 11:24:55 +0700

    Update to 0.1.1

--- src/Components/DiggerOrderComponent.jsx
+++ src/Components/DiggerOrderComponent.jsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { Table, DatePicker, Select, Button } from 'antd';
+import { Table, DatePicker, Select, Button, Modal, message } from 'antd';
 import { ApiFetch } from '../Helpers/Helpers';
 import Moment from 'moment';
-let ActionHistoryMap = new Map();
 export default function DiggerOrderComponent(props) {
-  const [DiggerOrdersTable, SetNewDiggerOrdersTable] = useState(null);
+  const [DiggerOrdersTable, SetNewDiggerOrdersTable] = useState([]);
   const [DiggerList, SetNewDiggerList] = useState(null);
   const [LoadTypesList, SetNewLoadTypesList] = useState(null);
+  const [SelectedKey, SetNewSelectedKey] = useState(null);
   const DiggerOrderHandler = (Action, Data, Index) => {
     let NewDiggerOrdersTable = [...DiggerOrdersTable];
     switch (Action) {
+      case 'Delete':
+        if ('Vehicle' in NewDiggerOrdersTable[SelectedKey]) {
+          ApiFetch(
+            `model/DiggerOrders/${NewDiggerOrdersTable[SelectedKey].VehicleId}/${NewDiggerOrdersTable[SelectedKey].TS}`,
+            'DELETE',
+            undefined,
+            (Response) => {
+              RequestDiggerOrders();
+              SetNewSelectedKey(null);
+            }
+          );
+        } else {
+          NewDiggerOrdersTable.splice(SelectedKey, 1);
+          SetNewDiggerOrdersTable(NewDiggerOrdersTable);
+          SetNewSelectedKey(null);
+        }
+        break;
       case 'ChangeDigger':
         NewDiggerOrdersTable[Index].VehicleId = Data;
-        NewDiggerOrdersTable[Index].Edited = true;
         SetNewDiggerOrdersTable(NewDiggerOrdersTable);
         break;
       case 'ChangeLoadType':
@@ -23,18 +39,58 @@ export default function DiggerOrderComponent(props) {
         break;
       case 'ChangeDate':
         NewDiggerOrdersTable[Index].TS = Data.format();
-        NewDiggerOrdersTable[Index].Edited = true;
         SetNewDiggerOrdersTable(NewDiggerOrdersTable);
+        break;
+      case 'AddDiggerOrder':
+        NewDiggerOrdersTable.push({
+          TS: Moment('08:00:00', 'HH:mm:ss').format(),
+          VehicleId: DiggerList[0].value,
+          LoadTypeId: LoadTypesList[0].value,
+          Edited: true,
+          Options: {},
+        });
+        SetNewDiggerOrdersTable(NewDiggerOrdersTable);
+        break;
+      case 'Cancel':
+        if ('Vehicle' in Data) {
+          ApiFetch(
+            `model/DiggerOrders/${Data.VehicleId}/${Data.TS}`,
+            'GET',
+            undefined,
+            (Response) => {
+              NewDiggerOrdersTable[Index] = Response.data;
+              SetNewDiggerOrdersTable(NewDiggerOrdersTable);
+            }
+          );
+        } else {
+          NewDiggerOrdersTable.splice(Index, 1);
+          SetNewDiggerOrdersTable(NewDiggerOrdersTable);
+        }
+
+        break;
+      case 'Save':
+        ApiFetch(
+          `model/DiggerOrders${
+            'Vehicle' in Data ? `/${Data.VehicleId}/${Data.TS}` : ''
+          } `,
+
+          `${'Vehicle' in Data ? 'PATCH' : 'POST'}`,
+          Data,
+          (Response) => {
+            RequestDiggerOrders();
+          }
+        ).catch(() => {
+          message.warning('Выберете другой экскаватор или дату для сохранения');
+        });
         break;
     }
   };
   const RequestDiggerOrders = () => {
     ApiFetch('model/DiggerOrders', 'GET', undefined, (Response) => {
       SetNewDiggerOrdersTable(
-        Response.data.map((Order, Index) => {
-          Order.Key = Index;
-          Order.Edited = false;
-          return Order;
+        Response.data.map((DiggerOrder) => {
+          DiggerOrder.Edited = false;
+          return DiggerOrder;
         })
       );
     });
@@ -71,10 +127,68 @@ export default function DiggerOrderComponent(props) {
   useEffect(RequestDiggerOrders, []);
   return (
     <>
+      <div
+        style={{
+          width: '200px',
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          marginBottom: '5px',
+        }}
+      >
+        <Button
+          size="small"
+          type="primary"
+          onClick={() => {
+            DiggerOrderHandler('AddDiggerOrder');
+          }}
+        >
+          Добавить
+        </Button>
+        <Button
+          size="small"
+          danger
+          type="primary"
+          onClick={() => {
+            if (SelectedKey != null) {
+              Modal.confirm({
+                title: 'Подтвердите действие',
+                content: 'Вы действитенльно хотите удалить объект?',
+                okText: 'Удалить',
+                cancelText: 'Отмена',
+                okButtonProps: { type: 'primary', danger: true, size: 'small' },
+                cancelButtonProps: { size: 'small' },
+                onOk: () => {
+                  DiggerOrderHandler('Delete');
+                },
+              });
+            }
+          }}
+        >
+          Удалить
+        </Button>
+      </div>
       <Table
+        rowSelection={{
+          columnWidth: 0,
+          selectedRowKeys: [SelectedKey],
+          hideSelectAll: true,
+          renderCell: () => {
+            return null;
+          },
+        }}
+        onRow={(Record) => {
+          return {
+            onClick: () => {
+              SetNewSelectedKey(Record['Key']);
+            },
+          };
+        }}
         pagination={false}
         rowKey="Key"
-        dataSource={DiggerOrdersTable}
+        dataSource={DiggerOrdersTable.map((DiggerOrder, Index) => {
+          DiggerOrder.Key = Index;
+          return DiggerOrder;
+        })}
         size="small"
         columns={[
           {
@@ -83,15 +197,18 @@ export default function DiggerOrderComponent(props) {
             dataIndex: 'TS',
             render: (Value, Record, Index) => {
               return (
-                <DatePicker
-                  showTime={true}
-                  onOk={(Moment) => {
-                    DiggerOrderHandler('ChangeDate', Moment, Index);
-                  }}
-                  size="small"
-                  value={Moment(Value)}
-                  format="DD.MM.YYYY HH:mm:ss"
-                />
+                <div style={{ cursor: 'pointer' }}>
+                  <DatePicker
+                    disabled={'Vehicle' in Record}
+                    showTime={true}
+                    onOk={(Moment) => {
+                      DiggerOrderHandler('ChangeDate', Moment, Index);
+                    }}
+                    size="small"
+                    value={Moment(Value)}
+                    format="DD.MM.YYYY HH:mm:ss"
+                  />
+                </div>
               );
             },
           },
@@ -101,14 +218,17 @@ export default function DiggerOrderComponent(props) {
             dataIndex: 'VehicleId',
             render: (Value, Record, Index) => {
               return (
-                <Select
-                  size="small"
-                  options={DiggerList}
-                  value={Value}
-                  onChange={(NewValue) => {
-                    DiggerOrderHandler('ChangeDigger', NewValue, Index);
-                  }}
-                />
+                <div style={{ cursor: 'pointer' }}>
+                  <Select
+                    disabled={'Vehicle' in Record}
+                    size="small"
+                    options={DiggerList}
+                    value={Value}
+                    onChange={(NewValue) => {
+                      DiggerOrderHandler('ChangeDigger', NewValue, Index);
+                    }}
+                  />
+                </div>
               );
             },
           },
@@ -118,7 +238,7 @@ export default function DiggerOrderComponent(props) {
             dataIndex: 'LoadTypeId',
             render: (Value, Record, Index) => {
               return (
-                <div style={{ display: 'flex' }}>
+                <div style={{ display: 'flex', cursor: 'pointer' }}>
                   <Select
                     size="small"
                     options={LoadTypesList}
@@ -134,10 +254,27 @@ export default function DiggerOrderComponent(props) {
                       justifyContent: 'space-evenly',
                     }}
                   >
-                    <Button type="primary" size="small">
-                      Сохранить
-                    </Button>
-                    <Button size="small">Отмена</Button>
+                    {Record.Edited ? (
+                      <>
+                        <Button
+                          type="primary"
+                          size="small"
+                          onClick={() => {
+                            DiggerOrderHandler('Save', Record);
+                          }}
+                        >
+                          Сохранить
+                        </Button>
+                        <Button
+                          size="small"
+                          onClick={() => {
+                            DiggerOrderHandler('Cancel', Record, Index);
+                          }}
+                        >
+                          Отмена
+                        </Button>
+                      </>
+                    ) : null}
                   </div>
                 </div>
               );
