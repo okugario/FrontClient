2679c7bc1e3dfb40f456f79c354f3c7bdca9e679
Mr.Amperage <Mr.Amperage@protonmail.com>
2021-08-26 15:02:09 +0700

    Update to 0.1.3c

--- src/Components/AccessRoleProfileComponent.jsx
+++ src/Components/AccessRoleProfileComponent.jsx
@@ -1,3 +1,47 @@
+import * as React from 'react';
+import { Input, Collapse } from 'antd';
+const { TextArea } = Input;
+const { Panel } = Collapse;
 export default function AccessRoleProfile() {
-  return 'Профиль роли';
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          paddingBottom: '10px',
+        }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>Роль:</div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Input
+            onChange={(Event) => {}}
+            size="small"
+            style={{ width: '160px' }}
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          paddingBottom: '10px',
+        }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>Пояснение:</div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <TextArea size="small" />
+        </div>
+      </div>
+      <Collapse>
+        <Panel header="Меню пользователя" key="UserMenu"></Panel>
+      </Collapse>
+      <Collapse>
+        <Panel header="Справочники" key="ConfigCategories"></Panel>
+      </Collapse>
+      <Collapse>
+        <Panel header="Участки" key="Regions"></Panel>
+      </Collapse>
+    </>
+  );
 }
